d51b0aba1fab182722bb5a915dba3cfabbd58bb3
achiquit <andre.chiquito@gmail.com>
2025-05-17 12:00:59 -0400

    Fixed the last-six script so it works perfectly now, working on the leaderboard script

--- scripts/last-six.sql
+++ scripts/last-six.sql
@@ -1,9 +1,20 @@
-SELECT ticks.id AS 'Tick ID', ticks.date AS 'Date', climbs.name AS 'Climb Name', climbs.grade AS 'Grade', ticks.pitches AS 'Pitches', ticks.height AS 'Height', ticks.style AS 'Style', ticks.success AS 'Success', partners.fname || ' ' || partners.lname AS 'Partner / Belayer', ticks.notes AS 'Notes'
-FROM
-    ticks
-    INNER JOIN climbs ON ticks.climb = climbs.id
-    INNER JOIN climbed_partners ON ticks.climbed_id = climbed_partners.id
-    LEFT JOIN climbed_with ON  climbed_partners.id = climbed_with.climbing_id
-    LEFT JOIN partners ON climbed_with.partner_id = partners.id
-ORDER BY ticks.id DESC
-LIMIT 30;
+SELECT tick_id AS 'Tick ID', tick_date AS 'Date', climb_name AS 'Climb Name', grade AS 'Grade', pitches AS 'Pitches', height as 'Height', style AS 'Style', success AS 'Success', group_concat(concat, ', ') AS 'Partner(s)', notes AS 'Notes'
+FROM (
+    SELECT ticks.id AS tick_id, ticks.date AS tick_date, climbs.name AS climb_name, climbs.grade AS grade, ticks.pitches AS pitches, ticks.height AS height, ticks.style AS style, ticks.success AS success, 
+    CASE 
+        WHEN climbed_id IS -1 THEN 'Guiding'
+        ELSE partners.fname || ' ' || partners.lname 
+        END AS concat, 
+    ticks.notes AS notes
+    FROM
+        ticks
+        INNER JOIN climbs ON ticks.climb = climbs.id
+        INNER JOIN climbed_partners ON ticks.climbed_id = climbed_partners.id
+        LEFT JOIN climbed_with ON  climbed_partners.id = climbed_with.climbing_id
+        LEFT JOIN partners ON climbed_with.partner_id = partners.id
+    ORDER BY ticks.id DESC
+    LIMIT 100
+)
+GROUP BY tick_id
+ORDER BY tick_id DESC
+LIMIT 6;
--- scripts/leaderboard.sql
+++ scripts/leaderboard.sql
@@ -1,3 +1,4 @@
+-- -- The old script that worked before I revamped the partners table setup
 -- SELECT
 --     (
 --         SELECT COUNT(date)
@@ -30,13 +31,31 @@
 -- ORDER BY total_height DESC
 -- LIMIT 5;
 
-SELECT
-    (
-        SELECT COUNT(DISTINCT(date))
-        FROM ticks
-        INNER JOIN climbed_partners ON ticks.climbed_id = climbed_partners.id
-        LEFT JOIN climbed_with ON  climbed_partners.id = climbed_with.climbing_id
-        LEFT JOIN partners ON climbed_with.partner_id = partners.id
-        WHERE
-    )
+-- -- This code gets a list of all my ticks attributed to each partner I climbed with (like the most recent ticks but without the partners aggregated into a single, collective tick)
+SELECT ticks.id, ticks.date, climbs.id, ticks.pitches, ticks.height, partners.id
 FROM
+    ticks
+    INNER JOIN climbs ON ticks.climb = climbs.id
+    INNER JOIN climbed_partners ON ticks.climbed_id = climbed_partners.id
+    JOIN climbed_with ON  climbed_partners.id = climbed_with.climbing_id
+    JOIN partners ON climbed_with.partner_id = partners.id
+ORDER BY ticks.id DESC
+LIMIT 30;
+
+
+-- -- My WIP script
+-- SELECT
+--     (
+--         SELECT COUNT(ticks.date)
+--         FROM
+--             ticks
+--             INNER JOIN climbed_partners ON ticks.climbed_id = climbed_partners.id
+--             JOIN climbed_with ON  climbed_partners.id = climbed_with.climbing_id
+--             JOIN partners ON climbed_with.partner_id = partners.id
+--         WHERE climbed_with.partner_id = partners.id
+--     )
+--     total_height
+-- FROM
+--     partners
+-- ORDER BY total_height DESC
+-- LIMIT 5;
