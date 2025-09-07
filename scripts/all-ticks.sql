287b354efeb57462ead413e627f77731584574a7
achiquit <andre.chiquito@gmail.com>
2025-09-07 09:37:14 -0400

    Tweaked all-ticks to include danger rating

--- scripts/all-ticks.sql
+++ scripts/all-ticks.sql
@@ -3,14 +3,26 @@ SELECT
     climbs.name AS 'Climb', 
     (
         CASE
-            WHEN climbs.commitment IS -1 THEN
+            WHEN climbs.danger IS -1 AND climbs.commitment IS -1 THEN
                 (SELECT group_concat(grades.grade, ', ')
                 FROM join_grades
                 INNER JOIN which_grades ON which_grades.id = join_grades.id
                 INNER JOIN grades ON grades.id = which_grades.grade
                 WHERE join_grades.id = climbs.grade)
-            ELSE
+            WHEN climbs.danger IS NOT -1 AND climbs.commitment IS -1 THEN
+                (SELECT group_concat(grades.grade, ', ') || ', ' || climbs.danger
+                FROM join_grades
+                INNER JOIN which_grades ON which_grades.id = join_grades.id
+                INNER JOIN grades ON grades.id = which_grades.grade
+                WHERE join_grades.id = climbs.grade)
+            WHEN climbs.danger IS -1 AND climbs.commitment IS NOT -1 THEN
                 (SELECT group_concat(grades.grade, ', ')  || ', Grade ' || climbs.commitment
+                FROM join_grades
+                INNER JOIN which_grades ON which_grades.id = join_grades.id
+                INNER JOIN grades ON grades.id = which_grades.grade
+                WHERE join_grades.id = climbs.grade)
+            ELSE
+                (SELECT group_concat(grades.grade, ', ') || ', ' || climbs.danger || ', Grade ' || climbs.commitment
                 FROM join_grades
                 INNER JOIN which_grades ON which_grades.id = join_grades.id
                 INNER JOIN grades ON grades.id = which_grades.grade
