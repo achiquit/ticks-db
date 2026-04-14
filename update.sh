5af3a97198637321d3b19589bc5426af5d4953db
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-04-14 12:38:28 -0400

    Automatic DB Update

--- update.sh
+++ update.sh
@@ -9,14 +9,14 @@ python3 scripts/add_data.py
 echo ""
 echo ""
 
-echo "echo "=+=+=+=+=+=+=+=--- Backing up DB to GitHub ---=+=+=+=+=+=+=+=""
+echo "=+=+=+=+=+=+=+=--- Backing up DB to GitHub ---=+=+=+=+=+=+=+="
 git commit -a -m "Automatic DB Update"
 git push
 
 echo ""
 echo ""
 
-echo "echo "=+=+=+=+=+=+=+=--- Sending data to the website directory ---=+=+=+=+=+=+=+=""
+echo "=+=+=+=+=+=+=+=--- Sending data to the website directory ---=+=+=+=+=+=+=+="
 cat scripts/send-to-website.sql | sqlite3 ticks
 
 cd ../websitejazzhands
