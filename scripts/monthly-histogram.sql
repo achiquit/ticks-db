f0806087a77081277873012f2741cf18b91380ea
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-05-13 20:29:35 -0400

    Automatic DB Update

--- scripts/monthly-histogram.sql
+++ scripts/monthly-histogram.sql
@@ -1,13 +1,21 @@
-WITH RECURSIVE months(month_date) AS (
-    SELECT date('now', 'start of month')
-    UNION ALL
-    SELECT date(month_date, '-1 month')
-    FROM months
-)
+-- WITH RECURSIVE months(month_date) AS (
+--     SELECT date('now', 'start of month')
+--     UNION ALL
+--     SELECT date(month_date, '-1 month')
+--     FROM months
+--     WHERE date(month_date) >= date('now', '-59 months')
+-- )
+-- SELECT 
+--     strftime('%Y-%m', month_date) AS month,
+--     IFNULL(SUM(height), 0) height
+-- FROM months
+-- LEFT OUTER JOIN ticks ON strftime('%Y-%m', ticks.date) = month
+-- GROUP BY month
+-- ORDER BY month ASC;
+
 SELECT 
-    strftime('%Y-%m', month_date) AS month,
-    IFNULL(SUM(height), 0) height
-FROM months
-LEFT OUTER JOIN ticks ON strftime('%Y-%m', ticks.date) = month
+    strftime('%Y-%m', date) AS month,
+    SUM(height) AS height
+FROM ticks
 GROUP BY month
 ORDER BY month ASC;
