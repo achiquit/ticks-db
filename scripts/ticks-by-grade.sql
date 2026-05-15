f54d7417211b4820b28f10205e6983220a2acc6e
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-05-15 17:39:19 -0400

    Automatic DB Update

--- scripts/ticks-by-grade.sql
+++ scripts/ticks-by-grade.sql
@@ -2,7 +2,7 @@ SELECT
     grades.id AS 'ID',
     grades.grade AS 'Grade',
     0 AS 'Count',
-    NULL as 'Type'
+    'NULL' as 'Type'
 FROM 
     grades
 WHERE grades.grade LIKE '5.%' AND grades.grade NOT LIKE '5.10' AND grades.grade NOT LIKE '5.11' AND grades.grade NOT LIKE '5.12' AND grades.grade NOT LIKE '5.13' AND grades.grade NOT LIKE '5.14' AND grades.grade NOT LIKE '5.15'
