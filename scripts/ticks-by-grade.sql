b593e891095944fd72dc8d83b66475a67e546601
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-05-15 17:38:36 -0400

    Automatic DB Update

--- scripts/ticks-by-grade.sql
+++ scripts/ticks-by-grade.sql
@@ -21,4 +21,20 @@ FROM
     LEFT JOIN climbs ON climbs.grade = join_grades.id
     LEFT JOIN ticks ON ticks.climb = climbs.id
 WHERE grades.grade LIKE '5.%' AND grades.grade NOT LIKE '5.10' AND grades.grade NOT LIKE '5.11' AND grades.grade NOT LIKE '5.12' AND grades.grade NOT LIKE '5.13' AND grades.grade NOT LIKE '5.14' AND grades.grade NOT LIKE '5.15' AND climbs.type = 4
+GROUP BY grades.grade
+
+UNION
+
+SELECT
+    grades.id AS 'ID',
+    grades.grade AS 'Grade',
+    COUNT(ticks.id) AS 'Count',
+    'Trad' AS 'Type'
+FROM
+    grades
+    INNER JOIN which_grades ON which_grades.grade = grades.id
+    INNER JOIN join_grades ON join_grades.id = which_grades.id
+    LEFT JOIN climbs ON climbs.grade = join_grades.id
+    LEFT JOIN ticks ON ticks.climb = climbs.id
+WHERE grades.grade LIKE '5.%' AND grades.grade NOT LIKE '5.10' AND grades.grade NOT LIKE '5.11' AND grades.grade NOT LIKE '5.12' AND grades.grade NOT LIKE '5.13' AND grades.grade NOT LIKE '5.14' AND grades.grade NOT LIKE '5.15' AND grades.grade NOT LIKE 'C%' AND grades.grade NOT LIKE '%Snow%' AND grades.grade NOT LIKE '%th%' AND climbs.type = 5 OR climbs.type = 12 AND grades.grade NOT LIKE 'C%' AND grades.grade NOT LIKE '%Snow%' AND grades.grade NOT LIKE '%th%' OR climbs.type = 13 AND grades.grade NOT LIKE 'C%' AND grades.grade NOT LIKE '%Snow%' AND grades.grade NOT LIKE '%th%'
 GROUP BY grades.grade;
