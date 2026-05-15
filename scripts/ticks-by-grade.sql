700e3b7ad30cb354e250ffb798eec1555c86273f
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-05-15 17:08:59 -0400

    Automatic DB Update

--- scripts/ticks-by-grade.sql
+++ scripts/ticks-by-grade.sql
@@ -7,6 +7,9 @@ FROM
     INNER JOIN join_grades ON join_grades.id = which_grades.id
     LEFT JOIN climbs ON climbs.grade = join_grades.id
     LEFT JOIN ticks ON ticks.climb = climbs.id
-WHERE grades.grade LIKE '5.%' AND grades.grade NOT LIKE '5.10' AND grades.grade NOT LIKE '5.11' AND grades.grade NOT LIKE '5.12' AND grades.grade NOT LIKE '5.13' AND grades.grade NOT LIKE '5.14' AND grades.grade NOT LIKE '5.15'
+WHERE grades.grade LIKE '5.%' AND grades.grade NOT LIKE '5.10' AND grades.grade NOT LIKE '5.11' AND grades.grade NOT LIKE '5.12' AND grades.grade NOT LIKE '5.13' AND grades.grade NOT LIKE '5.14' AND grades.grade NOT LIKE '5.15' AND climbs.type = 4
 GROUP BY grades.grade
 ORDER BY grades.id ASC;
+
+-- trad: 5, 12, 13
+-- sport: 4
