67a30729152cb53165039205ee1424336f885e81
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-05-15 17:12:09 -0400

    Automatic DB Update

--- scripts/db_magic.sql
+++ scripts/db_magic.sql
@@ -26,5 +26,8 @@
 .output data/climb-locs.csv
 .read scripts/climbs-map.sql
 
-.output data/ticks-by-grade.csv
-.read scripts/ticks-by-grade.sql
+.output data/ticks-by-grade-sport.csv
+.read scripts/ticks-by-grade-sport.sql
+
+.output data/ticks-by-grade-trad.csv
+.read scripts/ticks-by-grade-trad.sql
