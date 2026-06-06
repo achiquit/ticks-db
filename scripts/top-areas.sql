0dd1c8991b8909d872f118b76120bf44428f48f3
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-06-06 14:53:11 -0400

    Automatic DB Update

--- scripts/top-areas.sql
+++ scripts/top-areas.sql
@@ -1,7 +1,7 @@
 SELECT
     areas.area_name AS 'Area',
-    SUM(ticks.height) AS 'Height',
     COUNT(DISTINCT ticks.date) AS 'Days',
+    SUM(ticks.height) AS 'Height',
     areas.state || ', ' || areas.country AS 'Location'
 FROM
     ticks
