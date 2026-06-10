52a95f7e0ff27c95437324bd4b74e5e46239155a
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-06-10 14:05:09 -0400

    Automatic DB Update

--- scripts/grades_by_date.sql
+++ scripts/grades_by_date.sql
@@ -6,4 +6,5 @@ FROM
     JOIN climbs ON climbs.id = ticks.climb
     JOIN join_grades ON join_grades.id = climbs.grade
     JOIN which_grades ON which_grades.id = join_grades.id
-    JOIN grades ON grades.id = which_grades.grade;
+    JOIN grades ON grades.id = which_grades.grade
+WHERE grades.grade LIKE '5.%' AND grades.grade NOT LIKE 'C%' AND grades.grade NOT LIKE '%Snow%' AND grades.grade NOT LIKE '%th%' AND climbs.type = 5 OR climbs.type = 12 AND grades.grade NOT LIKE 'C%' AND grades.grade NOT LIKE '%Snow%' AND grades.grade NOT LIKE '%th%' OR climbs.type = 13 AND grades.grade NOT LIKE 'C%' AND grades.grade NOT LIKE '%Snow%' AND grades.grade NOT LIKE '%th%';
