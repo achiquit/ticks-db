ef37e769a150f5b8af8da14dee1ee533ba34f17a
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-06-15 22:31:33 -0400

    Automatic DB Update

--- scripts/leaderboard.sql
+++ scripts/leaderboard.sql
@@ -12,7 +12,4 @@ SELECT 'Guiding' AS 'Partner', COUNT(DISTINCT ticks.date) AS 'Days', SUM(ticks.p
 FROM
     ticks
     INNER JOIN climbs ON ticks.climb = climbs.id
-    INNER JOIN climbed_partners ON ticks.climbed_id = climbed_partners.id
-    JOIN climbed_with ON  climbed_partners.id = climbed_with.climbing_id
-    JOIN partners ON climbed_with.partner_id = partners.id
-WHERE ticks.climbed_id = -1 AND ticks.guided_id != -1;
+WHERE ticks.guided_id > -1;
