c9a0261b56ade67ab4497ea0d9a40ad287d56b5f
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-08-16 18:49:33 -0600

    Automatic DB Update

--- scripts/plotly_magic.py
+++ scripts/plotly_magic.py
@@ -257,7 +257,7 @@ def heatmap() -> None:
         histfunc='sum',
         # radius=8,
         # center=dict(lat=33, lon=320), zoom=1.4,
-        map_style="carto-darkmatter",
+        # map_style="carto-darkmatter",
         hover_data=dict(
             Latitude=False,
             Longitude=False,
