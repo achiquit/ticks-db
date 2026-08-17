f55d137fdc9a1ef2e0b707cd82612d261d69a4cb
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-08-16 18:40:10 -0600

    Automatic DB Update

--- scripts/plotly_magic.py
+++ scripts/plotly_magic.py
@@ -268,7 +268,7 @@ def heatmap() -> None:
         ),
         color_continuous_scale=["#ff2b6d", "#9810fa", emerald],
         range_color=[0,2],
-        histfunc='sum'
+        histfunc='count'
     )
     
 
